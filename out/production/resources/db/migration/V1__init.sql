599c97ccf3b7a0393f371a949a8f1ff68719c808
Konrad Czaja <czajak1995@gmail.com>
2019-03-20 22:50:40 +0100

    Added .gitignore. Cleaning cached files

--- out/production/resources/db/migration/V1__init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `Tracks`
-(
-    `id` integer NOT NULL AUTO_INCREMENT,
-    `name` varchar(30) NOT NULL,
-    `path` varchar(90) NOT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE KEY `TracksUnique` (`name`)
-);
