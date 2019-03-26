4957fa4e7f9400365b31f5ffef79ebc156fcafa0
Konrad Czaja <czajak1995@gmail.com>
2019-03-26 21:34:04 +0100

    object oriented approach for glyps

--- frontend/fajstos/src/app/app.component.ts
+++ frontend/fajstos/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { range } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,15 +8,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fajstos';
-  numbers = []
+  glyphs :PhaistosGlyph[] = [];
+  imgUrlTemplate: string = "assets/icons/Phaistos_glyph_{{number}}.svg.png";
+  glyphNumbers : number = 16;
 
   constructor() {
-    this.numbers = Array(16).fill(0).map((x,i)=>i+1);
+
+    for(var i=1; i<this.glyphNumbers; i++) {
+      if(i < 10) {
+        this.glyphs.push(new PhaistosGlyph(i,"","assets/icons/Phaistos_glyph_0" + i + ".svg.png"));
+      } else {
+        this.glyphs.push(new PhaistosGlyph(i,"","assets/icons/Phaistos_glyph_" + i + ".svg.png"));
+      }
+    }
+
+
+    // this.numbers = Array(16).fill(0).map(
+    //   (x,i)=>
+    //   {
+    //     new PhaistosGlyph(i+1,"","");
+    //   }
   }
 
   
 
   public display(elem : string) {
     console.log(elem)
+  }
+}
+
+class PhaistosGlyph {
+  id: number;
+  mapping: string;
+  imgUrl: string;
+
+  constructor(id: number, mapping: string, imgUrl: string) {
+    this.id = id;
+    this.mapping = mapping;
+    this.imgUrl = imgUrl;
   }
 }
